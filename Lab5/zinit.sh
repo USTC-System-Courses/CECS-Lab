a3d7921b16a2477c93e3e9f9254b9911246752e2
Ma Zi Rui <96678483+MaZirui2001@users.noreply.github.com>
2023-09-24 20:45:06 +0800

    Update zinit.sh

--- Lab5/zinit.sh
+++ Lab5/zinit.sh
@@ -1,7 +1,12 @@
 
-rm -rf ./mycpu ./script ./sim.cpp
+rm -rf ./mycpu ./script ./paddr.cpp ./sim.cpp
 
 
 ln -s ../simulator/IP/mycpu mycpu
 ln -s ../simulator/script script
+ln -s ../simulator/sim/memory/paddr.cpp paddr.cpp
 ln -s ../simulator/sim/sim.cpp sim.cpp
+
+cd ../simulator/IP
+
+git clone git@github.com:USTC-System-Courses/CECS-pipeline-AXI.git pipeline-AXI
