06ad942c5cdf164869aad9918769ed3170201f9f
Ma Zi Rui <96678483+MaZirui2001@users.noreply.github.com>
2023-09-24 20:45:32 +0800

    Update zinit.sh

--- Lab6/zinit.sh
+++ Lab6/zinit.sh
@@ -1,8 +1,9 @@
 
-rm -rf ./mycpu ./script ./sim.cpp ./base
+rm -rf ./mycpu ./script ./sim.cpp ./base ./device-test
 
 
 ln -s ../simulator/IP/mycpu mycpu
 ln -s ../simulator/script script
 ln -s ../simulator/sim/sim.cpp sim.cpp
 ln -s ../software/base-port/base base
+ln -s ../software/device-test device-test
