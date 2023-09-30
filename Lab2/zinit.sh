8df3be316e94882ae99ede28d1cd76fe1e447d59
__NYA__ <squirrel@mail.ustc.edu.cn>
2023-09-30 11:31:42 +0800

    lab2

--- Lab2/zinit.sh
+++ Lab2/zinit.sh
@@ -3,4 +3,5 @@ rm -rf ./sim.cpp ./build.cpp
 
 ln -s ../simulator/sim/sim.cpp sim.cpp
 ln -s ../simulator/sim/build.cpp build.cpp
+ln -s ../simulator/sim/main.cpp main.cpp
 ln -s ../simulator/sim/sdb sdb
